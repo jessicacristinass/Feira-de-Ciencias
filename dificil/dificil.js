4ce227f886eae25ea82198b593ef08517316b95e
Jessie-Cristina <136021045+Jessie-Cristina@users.noreply.github.com>
2023-06-18 18:51:13 -0300

    Update dificil.js

--- dificil/dificil.js
+++ dificil/dificil.js
@@ -183,7 +183,7 @@ function fim(){
 }
 
 document.getElementById("congrats").addEventListener("click", function() {
-  window.location.href = "/index.html"
+  window.location.href = "/Feira-de-Ciencias/"
 });
 
 var modal = document.getElementById("modal")
