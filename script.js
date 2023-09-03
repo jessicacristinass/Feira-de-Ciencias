9eee49ad9f274fbbab920c54e4d505d93a582ba2
Jessica Cristina da Silva Souza <136021045+jessicacristinass@users.noreply.github.com>
2023-09-03 16:17:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function conferir(){
   if (c == "-") {
     res = parseInt(a) - parseInt(b) 
   }else if(c == "÷"){
-    res = parseInt(parseInt(a)/parseInt(b))
+    res = (parseInt(a)/parseInt(b)).toFixed(2)
   }else if(c == "+"){
     res = parseInt(a) + parseInt(b) 
   }else if(c == "×"){
