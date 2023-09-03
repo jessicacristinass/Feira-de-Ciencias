b745adc98f1b33494755eaf428cc858573c77f30
Jessica Cristina da Silva Souza <136021045+jessicacristinass@users.noreply.github.com>
2023-09-03 17:36:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function conferir(){
     res = "outra operação"
   }
   
-  if (parseInt(document.getElementById("resp").value)==res){
+  if (parseInt(document.getElementById("resp").value) == res){
 
     var congrats = document.getElementById("congrats")
     
